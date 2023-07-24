b5fc868b3d99a426646edbe84267c4a71064dee3
sspsk <karapsot@gmail.com>
2023-07-24 21:18:01 +0300

    add best/check/last model checkpointing, add ability to continue from checkpoint

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import argparse
 import torch
 from torch.utils.data import DataLoader
 import sys
+import os
 
 
 parser = argparse.ArgumentParser()
@@ -41,16 +42,37 @@ model = model.to(DEVICE)
 optimizer = model.get_optimizer()
 criterion = model.get_criterion()
 
+best_val_loss = float('inf')
+epochs_done=0
 
 checkpoint = cfg['training'].get("checkpoint",None)
 if checkpoint is not None:
-    pass
+
     #load model & optimizer
+    print("Loading checkpoint from:",checkpoint)
+    chkpt = torch.load(checkpoint)
+
+    model.load_state_dict(chkpt['model_state_dict'])
+    optimizer.load_state_dict(chkpt['optimizer_state_dict'])
+
+    epochs_done = chkpt['epochs']
+
+    train_loss = chkpt['train_loss']
+    val_loss = chkpt['val_loss']
+    best_val_loss = chkpt['best_val_loss']
+
+    print("Epochs done:",epochs_done)
+    print("Previous Train Loss:",train_loss)
+    print("Previous Val Loss:",val_loss)
+    print("Previous Best Val Loss:",best_val_loss)
+
 
 
 epochs = cfg['training']['epochs']
+final_epoch = epochs_done+epochs
+
 train_running_loss = 0.0
-for ep in range(1,1+epochs):
+for ep in range(epochs_done+1,epochs_done + 1+ epochs):
 
     model.train()
 
@@ -68,9 +90,11 @@ for ep in range(1,1+epochs):
         optimizer.step()
 
         train_running_loss += loss.detach().cpu()
-        if batch_num % 10 == 0:
+        if batch_num % 4 == 0:
             print("Epoch: {0}, {1}/{2}, Loss: {3}".format(ep,batch_num,len(train_loader),train_running_loss/batch_num))
 
+    train_loss = train_running_loss/batch_num
+    print("Epoch {0}: Train Loss: {1}".format(ep,train_loss))
 
     model.eval()
     val_running_loss = 0.0
@@ -87,14 +111,36 @@ for ep in range(1,1+epochs):
 
             print("Val {0}/{1}, Loss: {2}".format(batch_num,len(val_loader),val_running_loss/batch_num))
     
-    
-    #if best val loss, checkpointing
-    #if epochs % 10 == 0 checkpointing
+    val_loss = val_running_loss/batch_num 
+    print("Epoch {0}: Val Loss: {1}".format(ep,val_loss))
 
+    if best_val_loss > val_loss or ep%10 == 0 or ep==final_epoch:
 
+        is_best = False
+        if best_val_loss > val_loss: 
+            best_val_loss=val_loss
+            is_best = True
+        
+        checkpoint_dict = {
+            "model_state_dict": model.state_dict(),
+            "optimizer_state_dict": optimizer.state_dict(),
+            "epochs": ep,
+            "train_loss": train_loss,
+            "val_loss": val_loss,
+            "best_val_loss": best_val_loss,
+        }
 
+        if is_best:
+            best_filename = os.path.join(curr_exp_dir,"best.pt")
+            print("Saving best model at:",best_filename)
+            torch.save(checkpoint_dict,best_filename)
+        if ep%10 == 0:
+            check_filename = os.path.join(curr_exp_dir,"check.pt")
+            print("Saving every-10-epochs checkpoint model at:",check_filename)
+            torch.save(checkpoint_dict,check_filename)
+        if ep==final_epoch:
+            last_filename = os.path.join(curr_exp_dir,"last.pt")
+            print("Saving last model at:",last_filename)
+            torch.save(checkpoint_dict,last_filename)
 
-#TODO
-#checkpoint loading
-#training loop
-#val loop
+print("Training completed.")
