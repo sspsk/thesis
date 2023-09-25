46bfb1bc9b2658cbd4c45477959fce0284ee16d2
sspsk <karapsot@gmail.com>
2023-09-26 00:17:03 +0300

    update hmr2 to use reprojection loss

--- models/hmr2.py
+++ models/hmr2.py
@@ -1,5 +1,6 @@
 import config
-from data.utils import load_mean_parameters,rot6d_to_rotmat
+import constants
+from data.utils import load_mean_parameters, orth_proj, reconstruction_error,rot6d_to_rotmat
 from models.backbone import CustomResNet
 from models.smpl import get_smpl_model
 
@@ -8,6 +9,7 @@ from torch.optim import Adam
 from torch import nn
 from torchvision.models import resnet50, ResNet50_Weights
 import numpy as np
+from smplx.lbs import vertices2joints
 
 
 class Regressor(nn.Module):
@@ -105,11 +107,13 @@ class HMR2(nn.Module):
         img = batch['img']
         pose_gt = batch['pose']
         shape_gt = batch['shape']
+        gt_kp = batch['keypoints2d']
+        vis = batch['visibility2d']
 
         if self.cfg['model'].get('gtshape',False):
-            _,pose_pred,shape_pred = self(img,shape=shape_gt)
+            cam_pred,pose_pred,shape_pred = self(img,shape=shape_gt)
         else:
-            _,pose_pred,shape_pred = self(img)
+            cam_pred,pose_pred,shape_pred = self(img)
 
         mat_pose = []
         mat_pose.append(rot6d_to_rotmat(pose_pred[-1].reshape(-1,6)).reshape(-1,24,3,3))
@@ -117,7 +121,6 @@ class HMR2(nn.Module):
         pose_loss = criterion[0](mat_pose[-1],pose_gt) 
         loss_dict['pose_loss'] = pose_loss
 
-        shape_loss = criterion[0](shape_pred[-1],shape_gt)
 
         res_pred = self.smpl(global_orient=mat_pose[-1].flatten(2,3)[:,:1,:],
                              body_pose=mat_pose[-1].flatten(2,3)[:,1:,:],
@@ -129,17 +132,23 @@ class HMR2(nn.Module):
                              betas=shape_gt,
                              pose2rot=False)
         
-        joints_loss = criterion[1](res_pred.joints[:,:24,:],res_gt.joints[:,:24,:]).sum([1,2]).mean()
+        joints_loss = criterion[1](res_pred.joints,res_gt.joints).sum([1,2]).mean()#3d keypoints loss on all 49 joints of custom smpl
         loss_dict['joints_loss'] = joints_loss
 
-        if self.cfg['model'].get('gtshape',False):
-            loss = pose_loss + joints_loss
-        else:
-            if  self.cfg['model'].get('with_shape_loss',True):
-                loss = pose_loss + shape_loss + joints_loss
-                loss_dict['shape_loss'] = shape_loss
-            else:
-                loss = pose_loss + joints_loss
+        loss = pose_loss + joints_loss
+
+        if  self.cfg['model'].get('with_shape_loss',True):
+            shape_loss = criterion[0](shape_pred[-1],shape_gt)
+            loss += shape_loss 
+            loss_dict['shape_loss'] = shape_loss
+
+        if self.cfg['model'].get('with_reprojection_loss',True):
+            pred_kp = orth_proj(res_pred.joints,cam_pred[-1])
+            #normalizing gt_kp to [-1,1]
+            gt_kp = gt_kp/(constants.IMG_RES/2) - 1.0
+            reprojection_loss = (criterion[1](pred_kp,gt_kp) * vis.unsqueeze(-1)).sum([1,2]).mean()
+            loss += reprojection_loss
+            loss_dict['reprojection_loss'] = reprojection_loss
 
 
         return loss, loss_dict
@@ -147,6 +156,10 @@ class HMR2(nn.Module):
         
     def validation_step(self,batch,criterion):
         #assume that batch and model are on the same device
+
+        if not hasattr(self,'j36m_regressor'):
+            self.j36m_regressor= torch.from_numpy(np.load(config.JOINT_REGRESSOR_H36M)).to(dtype=torch.float32)
+
         loss_dict = {}
 
         img = batch['img']
@@ -171,7 +184,16 @@ class HMR2(nn.Module):
                              betas=shape_gt,
                              pose2rot=False)
         
-        loss = criterion[1](res_pred.joints[:,:24,:],res_gt.joints[:,:24,:]).sum([1,2]).mean()
+        #loss = criterion[1](res_pred.joints[:,:24,:],res_gt.joints[:,:24,:]).sum([1,2]).mean()
+        pred_vertices = res_pred.vertices   
+        pred_h36m_joints = vertices2joints(self.j36m_regressor.to(pred_vertices.device),pred_vertices)
+        pred_joints = pred_h36m_joints[:,constants.H36M_TO_J14,:]
+
+        gt_vertices = res_gt.vertices   
+        gt_h36m_joints = vertices2joints(self.j36m_regressor.to(pred_vertices.device),gt_vertices)
+        gt_joints = gt_h36m_joints[:,constants.H36M_TO_J14,:]
+
+        loss =  torch.tensor(reconstruction_error(pred_joints.cpu().numpy(),gt_joints.cpu().numpy(),reduction='mean'))
         loss_dict['joints_loss'] = loss
 
 
@@ -217,6 +239,10 @@ class HMR2(nn.Module):
     
     def get_criterion(self):
         #can be a single criterion or a list of them
-        criterion1 = nn.MSELoss()
-        criterion2 = nn.MSELoss(reduction='none')
+        if self.cfg['model'].get('l1_loss',False):
+            criterion1 = nn.L1Loss()
+            criterion2 = nn.L1Loss(reduction='none')
+        else:
+            criterion1 = nn.MSELoss()
+            criterion2 = nn.MSELoss(reduction='none')
         return [criterion1,criterion2]
